8068639a5cc34a69a316c1e8695290654ac7fc78
Robert Basamac <rbasamac@gmail.com>
2023-05-03 21:42:35 +0300

    Add a list of words

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Robert Basamac on 03.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var usedWords: [String] = [String]()
+    @State private var rootWord: String = ""
+    @State private var newWord: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit {
+                addNewWord()
+            }
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
