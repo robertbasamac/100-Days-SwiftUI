1dfaa58a5ba461cdb21d6f426ca4ad50f64d42f2
Robert Basamac <rbasamac@gmail.com>
2023-05-17 14:59:42 +0300

    Merge Codable structs and display a MissionView

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(missions) { mission in
                         NavigationLink {
-                            Text("Defail View")
+                            MissionView(mission: mission, astronauts: astronauts)
                         } label: {
                             VStack {
                                 Image(mission.image)
