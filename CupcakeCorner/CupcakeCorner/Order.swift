b9e8f9bc7b266063ec6f71e981da405e205206dc
Robert Basamac <rbasamac@gmail.com>
2023-06-15 15:46:51 +0300

    Implement Coding protocol to Order class

--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -7,7 +7,12 @@
 
 import SwiftUI
 
-final class Order: ObservableObject {
+final class Order: ObservableObject, Codable {
+    
+    enum CodingKeys: CodingKey {
+        case type, quantity, extraFrosting, addSprinkles, name, streetAddress, city, zip
+    }
+    
     static let types: [String] = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
     @Published var type: Int = 0
@@ -53,5 +58,37 @@ final class Order: ObservableObject {
         }
         
         return cost
+    }
+    
+    init() { }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+        
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+                
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+
+        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+
+        name = try container.decode(String.self, forKey: .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
     }
 }
