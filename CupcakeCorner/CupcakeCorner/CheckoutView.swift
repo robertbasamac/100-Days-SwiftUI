8b201187b7c93769e98d6391f8eb68f995b0fc23
Robert Basamac <rbasamac@gmail.com>
2023-06-15 16:03:30 +0300

    Sending and receiving the order over the internet using a free API

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -11,6 +11,9 @@ struct CheckoutView: View {
     
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage: String = ""
+    @State private var showingConfirmation: Bool = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -27,13 +30,50 @@ struct CheckoutView: View {
                     .font(.title)
                 
                 Button("Place order") {
-                    
+                    Task {
+                        await placeOrder()
+                    }
                 }
                 .buttonStyle(.borderedProminent)
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button(action: {}, label: {
+                Text("OK")
+            })
+        } message: {
+            Text(confirmationMessage)
+        }
+
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order.")
+            return
+        }
+        
+        guard let url = URL(string: "https://reqres.in/api/cupcakes") else {
+            print("Could not create the url.")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
     }
 }
 
